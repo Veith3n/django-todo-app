7547dde4f69754cb91f05175b2cd715617fcfd93
Krzysztof Kopydłowski <28898861+Veith3n@users.noreply.github.com>
2021-01-17 02:49:00 +0100

    Implement CRUD actions for task model

--- todos/urls.py
+++ todos/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("tasks", views.create, name="create_task"),
+    path("tasks/<int:pk>/", views.update, name="update_task"),
+    path("tasks/<int:pk>/del", views.delete, name="delete_task"),
+]
--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,49 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from .forms import TaskForm
+from .models import Task
 
 
 def index(request):
-    return render(request, "index.html")
+    tasks = Task.objects.all()
+
+    return render(request, "tasks/index.html", {"tasks": tasks})
+
+
+def create(request):
+    form = TaskForm()
+    if request.method == "POST":
+        return _handle_form_update(req=request)
+
+    return render(request, "tasks/create.html", {"task_form": form})
+
+
+def update(request, pk):
+    try:
+        task = Task.objects.get(id=pk)
+        form = TaskForm(instance=task)
+        if request.method == "POST":
+            return _handle_form_update(req=request, instance=task)
+
+        return render(request, "tasks/update.html", {"task_edit_form": form})
+
+    except:
+        Task.DoesNotExist
+    return redirect("index")
+
+
+def delete(request, pk):
+    try:
+        task = Task.objects.get(id=pk)
+        task.delete()
+        return redirect("index")
+
+    except:
+        Task.DoesNotExist
+    return redirect("index")
+
+
+def _handle_form_update(req, instance=None):
+    form = TaskForm(req.POST) if instance is None else TaskForm(req.POST, instance=instance)
+    if form.is_valid():
+        form.save()
+        return redirect("index")
